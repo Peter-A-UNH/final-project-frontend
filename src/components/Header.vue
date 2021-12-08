b1e4cfcedf9d186d3cbb5fcac99e13e9f0e1c7c7
Peter Aurigemma <pa@puter.local>
2021-12-07 22:45:46 -0500

    Added Pizza pic and cleaned up CSS

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,11 +1,9 @@
 <template>
     <div>
+        <img src="https://images.unsplash.com/photo-1542834369-f10ebf06d3e0?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2670&q=80" alt="Pizza">
         <h1>
-            {{restaurantName}}
+            {{title}}
         </h1>
-        <h3>
-            {{restaurantCity}}
-        </h3>
     </div>
 </template>
 
@@ -14,10 +12,8 @@
 export default {
     name: 'Header',
     props:{
-        restaurantName: String,
-        restaurantCity: String
+        title: String,
     }
-
 }
 
 </script>
@@ -26,7 +22,14 @@ export default {
 
 div{
     text-align: center;
+    border-radius: 10px;
+    padding: 1em;
+    background-color: rgb(50, 231, 44);
+    border: 1em solid rgb(240, 10, 144);
 }
-
+img {
+    max-width:65%;
+    height:auto;
+}
 
 </style>
